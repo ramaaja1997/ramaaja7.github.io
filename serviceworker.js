0fa6f0da905ba258e96fc36672de6981c749e906
ramaaja1997 <ramaaja1997@gmail.com>
2019-04-15 08:30:00 +0700

    first commit

---
+++ serviceworker.js
@@ -0,0 +1,55 @@
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function(err) {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err);
+      });
+    });
+  }
+
+  var CACHE_NAME = 'my-site-cache-v1';
+  var urlsToCache = [
+  '/',
+  'index.html',
+  'js/bootstrap.js',
+  'css/bootstrap.css',
+  'images/icons/icon-72x72.png',
+  'images/icons/icon-96x96.png',
+  'images/icons/icon-128x128.png',
+  'images/icons/icon-144x144.png',
+  'images/icons/icon-152x152.png',
+  'images/icons/icon-192x192.png',
+  'images/icons/icon-384x384.png',
+  'images/icons/icon-512x512.png',
+  'images/logo.png',
+  'manifest.json',
+  'serviceworker.js'
+  ];
+  
+  self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+  });
+  
+  self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+  });
